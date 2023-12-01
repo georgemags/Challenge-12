3d3736d95d931d5c007fea58cdc90f0eee61d2c6
George Maguire <georgemaguire99@gmail.com>
2023-11-30 22:41:33 -0600

    commit 3

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: "password"
+  database: 'employees_db'
+});
